ec3ea1eab05b8ba06a08e2a47e7b627bc10acc58
Chingiz Tuleubayev <59221853+chingizof@users.noreply.github.com>
2023-04-11 23:07:59 +0600

    Update index.jsx

--- frontend/src/components/scoreboard/index.jsx
+++ frontend/src/components/scoreboard/index.jsx
@@ -51,11 +51,6 @@ export const Scoreboard = ({score, setStartTime, setGameTime}) => {
             {scores?.map((score) => (
                 <ScoreboardItem name={score.name} score={score.score} />
             ))}
-                {/* <ScoreboardItem name="Murat" score={score} />
-                <ScoreboardItem name="Beknar" score="49" />
-                <ScoreboardItem name="Aidar" score="58" />
-                <ScoreboardItem name="Zhanibek" score="19" />
-                <ScoreboardItem name="Nartay" score="120" /> */}
         </div>
     )
 }
